f2e5cdbfe22fa18ace94927e833f9aaa6261f690
aymane <aymanerachid47@gmail.com>
2024-12-07 00:02:49 +0100

    added bg background and fixed issue console

--- my-project/src/components/AboutMe.jsx
+++ my-project/src/components/AboutMe.jsx
@@ -1,150 +1,27 @@
 
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+
 
 const AboutMe = (params) => {
   return(
      <>
-       <div className="flex flex-col">
-           <h1>About Me </h1>
-           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam assumenda natus dolorem labore voluptas sunt atque ab nisi. Dolorem cumque iusto tenetur dolor id possimus voluptas blanditiis amet explicabo excepturi odit aliquid exercitationem eligendi maiores dolores fugiat expedita at, neque natus et libero. Facilis, nobis.</p>
-           <div className=" flex ">
-                {/* Success - Solid */}
-
-<span
-  className="inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700"
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth="1.5"
-    stroke="currentColor"
-    className="-ms-1 me-1.5 size-4"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+       <div className="flex lg:justify-between lg:px-24 ">
+        <div className="w-4/12">
+        <DotLottieReact
+      src="https://lottie.host/6d88acd8-97de-49ba-bbfb-ffaaaa673ae5/FD7zzK8PXy.lottie"
+      loop
+      autoplay
     />
-  </svg>
+        </div>
+         <div className="w-1/2 flex flex-col gap-5">
+         <h1 className='text-6xl'>About Me </h1>
+     
+        <p className='text-md'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam assumenda natus dolorem labore voluptas sunt atque ab nisi. Dolorem cumque iusto tenetur dolor id possimus voluptas blanditiis amet explicabo excepturi odit aliquid exercitationem eligendi maiores dolores fugiat expedita at, neque natus et libero. Facilis, nobis.</p>
+         </div>
+   
 
-  <p className="whitespace-nowrap text-sm">Paid</p>
-</span>
-
-{/* Success - Outlined */}
-
-<span
-  className="inline-flex items-center justify-center rounded-full border border-emerald-500 px-2.5 py-0.5 text-emerald-700"
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth="1.5"
-    stroke="currentColor"
-    className="-ms-1 me-1.5 size-4"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-    />
-  </svg>
-
-  <p className="whitespace-nowrap text-sm">Paid</p>
-</span>
-
-{/* Warning - Solid */}
-
-<span
-  className="inline-flex items-center justify-center rounded-full bg-amber-100 px-2.5 py-0.5 text-amber-700"
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth="1.5"
-    stroke="currentColor"
-    className="-ms-1 me-1.5 size-4"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M8.25 9.75h4.875a2.625 2.625 0 010 5.25H12M8.25 9.75L10.5 7.5M8.25 9.75L10.5 12m9-7.243V21.75l-3.75-1.5-3.75 1.5-3.75-1.5-3.75 1.5V4.757c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0c1.1.128 1.907 1.077 1.907 2.185z"
-    />
-  </svg>
-
-  <p className="whitespace-nowrap text-sm">Refunded</p>
-</span>
-
-{/* Warning - Outlined  */}
-
-<span
-  className="inline-flex items-center justify-center rounded-full border border-amber-500 px-2.5 py-0.5 text-amber-700"
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth="1.5"
-    stroke="currentColor"
-    className="-ms-1 me-1.5 size-4"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M8.25 9.75h4.875a2.625 2.625 0 010 5.25H12M8.25 9.75L10.5 7.5M8.25 9.75L10.5 12m9-7.243V21.75l-3.75-1.5-3.75 1.5-3.75-1.5-3.75 1.5V4.757c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0c1.1.128 1.907 1.077 1.907 2.185z"
-    />
-  </svg>
-
-  <p className="whitespace-nowrap text-sm">Refunded</p>
-</span>
-
-{/* Error- Solid */}
-
-<span
-  className="inline-flex items-center justify-center rounded-full bg-red-100 px-2.5 py-0.5 text-red-700"
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth="1.5"
-    stroke="currentColor"
-    className="-ms-1 me-1.5 size-4"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-    />
-  </svg>
-
-  <p className="whitespace-nowrap text-sm">Failed</p>
-</span>
-
-{/* Error - Outlined */}
-
-<span
-  className="inline-flex items-center justify-center rounded-full border border-red-500 px-2.5 py-0.5 text-red-700"
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth="1.5"
-    stroke="currentColor"
-    className="-ms-1 me-1.5 size-4"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-    />
-  </svg>
-
-  <p className="whitespace-nowrap text-sm">Failed</p>
-</span>
-           </div>
+          
        </div>
      </>
   )
